8ab15aae93d82d7fe438a59917b083a693dd761d
Pravin <mp.pravinkanna@gmail.com>
2025-01-26 13:30:11 +0530

    Refactored main file

--- cmd/imdbscraper/main.go
+++ cmd/imdbscraper/main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"slices"
 	"strings"
+	"time"
 
 	"github.com/pravinkanna/imdb-scraper/scraper"
 )
@@ -13,11 +15,41 @@ import (
 func main() {
 	ctx := context.Background()
 
-	// Fetch the Genres from IMDB
+	scrapeID := createResultDirectory()
+
+	// Fetch the list of Genres from IMDB
 	genres, err := scraper.GetGenres()
 	if err != nil {
 		fmt.Println("Failed to fetch page")
 	}
+
+	searchConfig := getUserInput(genres)
+
+	// Function to Scrape the movie details and store it to the SQLite DB
+	err = scraper.ScrapeMovies(ctx, searchConfig, scrapeID)
+	if err != nil {
+		log.Fatalln("Error scraping movies:", err)
+	}
+}
+
+// Function to Generate the scrapeID and create result directory
+func createResultDirectory() string {
+	// Create a result directory with a unique id
+	t := time.Now()
+	scrapeID := t.Format("20060102150405")
+	resultPath := fmt.Sprintf("./results/%s/", scrapeID)
+	if _, err := os.Stat(resultPath); os.IsNotExist(err) {
+		err := os.Mkdir(resultPath, os.ModePerm)
+		if err != nil {
+			log.Fatalf("Failed to create result directory: %+v", err)
+		}
+	}
+
+	return scrapeID
+}
+
+// Function to get the filters from user (i.e) Genre and keyword
+func getUserInput(genres []string) scraper.SearchConfig {
 
 	// Get the Genre from user and validate against the fetched Genres
 	var genre string
@@ -39,8 +71,5 @@ func main() {
 
 	// Title is set as "feature" to filter only the movies
 	searchConfig := scraper.SearchConfig{Title: "feature", Genre: genre, Keyword: keyword}
-	err = scraper.ScrapeMovies(ctx, searchConfig)
-	if err != nil {
-		log.Fatalln("Error scraping movies:", err)
-	}
+	return searchConfig
 }
